5258cb34ed369384523550ccfdbb4bd06f73999b
not2nder <brtiodark@gmail.com>
2026-05-25 20:33:10 -0300

    Feat: Sistema de temas melhorado

--- util/theme.py
+++ util/theme.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import tomllib
 
+CURRENT_THEME = None
+
 class Theme:
     def __init__(self, colors):
         self.bg = colors["bg"]
@@ -9,14 +11,22 @@ class Theme:
         self.secondary_bg = colors.get("secondary_bg", self.bg)
         self.secondary_fg = colors.get("secondary_fg", self.fg)
         
-        self.statusline_bg = colors.get("statusline_bg", self.secondary_bg)
-        self.statusline_fg = colors.get("statusline_fg", self.secondary_fg)
+        self.status_bg = colors.get("statusline_bg", self.secondary_bg)
+        self.status_fg = colors.get("statusline_fg", self.secondary_fg)
 
-        self.hg_bg = colors.get("highlight_bg", self.secondary_bg)
-        self.hg_fg = colors.get("highlight_fg", self.secondary_fg)
+        self.iline_bg = colors.get("indicator_line_bg", self.secondary_bg)
+        self.iline_fg = colors.get("indicator_line_fg", self.secondary_fg)
 
         self.index_bg = colors.get("index_bg", self.bg)
-        self.index_fg = colors.get("index_fg", self.secondary_fg)
+        self.index_fg = colors.get("index_fg", self.fg)
+
+        self.time_fg = colors.get("time_fg", self.bg)
+
+        self.inum_bg = colors.get("indicaor_num_bg", self.bg)
+        self.inum_fg = colors.get("indicator_num_fg", self.fg)
+
+        self.warning_bg = colors.get("warning_bg", self.secondary_bg)
+        self.warning_fg = colors.get("warning_fg", self.secondary_fg)
 
 def load_theme(name):
     theme_file = Path.home()/".config"/"vi-player"/"themes"/f"{name}.toml"
@@ -26,12 +36,10 @@ def load_theme(name):
 
     return Theme(theme["colors"])
 
-def get_current_theme():
-    config_path = Path.home()/".config"/"vi-player"
-    
-    with open(config_path/"config.toml", "rb") as f:
-        config = tomllib.load(f)
+def set_theme(name):
+    global CURRENT_THEME
+    CURRENT_THEME = load_theme(name)
 
-    current_theme = config["general"]["theme"]
-
-    return load_theme(current_theme)
+def get_theme():
+    global CURRENT_THEME
+    return CURRENT_THEME
