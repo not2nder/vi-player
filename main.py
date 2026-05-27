d75716a2bb84bc9cf3138f7a8216e76ea4aa8489
not2nder <brtiodark@gmail.com>
2026-05-27 19:39:39 -0300

    Feat: adicionada a navegação por meio de motions

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import sys, tty, termios
 import shlex
-from pathlib import Path
-
-import mpv
 
 from util import ui
-from util.pretty import length
 from util.theme import set_theme
-from util.config import load_config, get_config 
+from util.config import load_config, get_config, save
 from util.screen import Screen
+from util.player import Player
 
 import signal
 
+player = Player()
+
 if len(sys.argv) > 1:
-    path = Path(sys.argv[1])
-    songs = list(path.glob("*.mp3"))
-else:
-    path = ""
-    songs = []
+    path = sys.argv[1]
+    player.load_songs(path)
+    songs = player.playlist
 
 load_config()
 config = get_config()
-set_theme(config.theme)
+set_theme(config.general["theme"])
 
 screen = Screen()
 
@@ -48,75 +45,18 @@ def draw():
     ui.draw_background(screen)
     ui.draw_header(path, screen=screen)
     ui.draw_songs(songs, indicator, screen=screen)
-    ui.draw_warning(warning, screen=screen)
+    ui.draw_warning(player.state, screen=screen)
     ui.draw_statusbar(mode, indicator+1, len(songs), screen=screen)
     ui.draw_commandline(command, screen=screen)
 
-    if mode == "COMANDO":
-        ui.show_cursor()
-        ui.move_cursor(screen.height, length(command)+1)
-    else:
-        ui.hide_cursor()
-
     screen.render()
-
-def play_current():
-    global warning
-    global current
-    global indicator
-
-    song = songs[indicator]
-    warning = "TOCANDO"
-    player.play(str(song))
-    current = indicator
-
-def next_song():
-    global current
-    global indicator
-
-    current = (current+1) % len(songs)
-    indicator = current
-    play_current()    
-
-def previous_song():
-    global current
-    global indicator
-
-    current = (current-1) % len(songs)
-    indicator = current
-    play_current()
-
-def pause():
-    global warning
-
-    if player.pause:
-        warning = "TOCANDO"
-    else:
-        warning = "PAUSA"
-
-    player.pause = not player.pause
-
-def skip(to: int = 1):
-    global current
-    global indicator
-
-    if not 1 <= to <= len(songs):
-        return
-    
-    current = to - 1
-    indicator = current
-
-    play_current()
-
+   
 ui.initscreen(screen)
 
 command = ""
 mode = "NORMAL"
-warning = "AGUARDANDO"
+motion = ""
 
-player = mpv.MPV(video=False)
-
-current = 0
 indicator = 0
 
 while True:
@@ -129,12 +69,21 @@ while True:
     key = getch()
 
     if mode == "NORMAL":
+        if key.isdigit():
+            motion+= key
+        elif (key == "j" or key == "k") and len(motion) >= 1:
+            num = int(motion)
+            if key == "j":
+                indicator = (indicator+num) % len(songs)
+            elif key == "k":
+                indicator = (indicator-num) % len(songs)
+            motion = ""
+            key = ""
+
         match key:
             case ":":
                 mode = "COMANDO"
                 command = ":"
-            case "p":
-                mode = "PLAYER"
             case "h":
                 indicator = 0
             case "l":
@@ -147,7 +96,7 @@ while True:
                 if len(songs) > 1:
                     indicator = (indicator-1) % len(songs)
             case "q":
-                player.terminate()
+                player.exit()
                 break
 
     elif mode == "COMANDO":
@@ -163,34 +112,38 @@ while True:
 
             match cmd:
                 case ":p":
-                    play_current()
+                    player.set_current(indicator)
+                    player.play()
                 case ":pp":
-                    pause()
+                    player.pause()
                 case ":sk":
-                    if len(args) < 2:
-                        skip(1)
-                        continue
-                    skip(int(args[1]))
-                case ":nx":
-                    next_song()
+                    player.skip(int(args[1]))
+                    indicator = player.get_current()
+                case ":n":
+                    player.next()
+                    indicator = player.get_current()
                 case ":pv":
-                    previous_song()
+                    player.prev()
+                    indicator = player.get_current()
                 case ":q":
-                    player.terminate()
+                    player.exit()
                     break
                 case ":o":
-                    path = Path(str(args[1])).expanduser()
-                    if path.exists():
-                        songs = list(path.glob("*.mp3"))
-                        indicator = 0
+                    songs = player.load_songs(str(args[1]))
+                    indicator = player.get_current()
                 case ":theme":
                     try:
                         set_theme(args[1])
-                        warning = "Tema da sessão atualizado!"
                     except:
-                        warning = f"Tema não encontrado: {args[1]}"
+                        pass
+                case ":set":
+                    attr = args[1]
+                    val = args[2]
+
+                    config.set_general(attr, val)
+                    if attr == "theme":
+                        set_theme(val)
                 case _:
-                    warning = f"Comando desconhecido: <{cmd}>"
                     pass
 
             command = ""
