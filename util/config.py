208a613fe9741ed22b63f23dbca9dbf3bb10b35b
not2nder <brtiodark@gmail.com>
2026-05-27 19:34:12 -0300

    Refactor: Config.py agora recebe .json

--- util/config.py
+++ util/config.py
@@ -1,21 +1,35 @@
 from pathlib import Path
-import tomllib
+import json
 
 CURRENT_CONFIG = None
+CONFIG_FILE = Path.home()/".config"/"vi-player"/"config.json"
 
 class Config:
     def __init__(self, data):
-        general = data.get("general", {})
-        self.theme = general.get("theme", "default")
+        self.general = data.get("general", {})
+        self.general["theme"] = self.general.get("theme", "default")
+
+    def set_theme(self, name):
+        self.general["theme"] = name
+
+    def to_dict(self):
+        return {
+            "general": self.general
+        }
 
 def load_config():
     global CURRENT_CONFIG
-    config_file = Path.home()/".config"/"vi-player"/"config.toml"
 
-    with open(config_file, "rb") as f:
-        config = tomllib.load(f)
+    with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        config = json.load(f)
 
     CURRENT_CONFIG = Config(config)
+
+def save():
+    global CURRENT_CONFIG
+
+    with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+        json.dump(CURRENT_CONFIG.to_dict(), f, indent=2)
 
 def get_config():
     return CURRENT_CONFIG
