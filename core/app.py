c195b65ca0c83b576286be5faa3c37adbc47d40f
not2nder <brtiodark@gmail.com>
2026-05-29 14:28:41 -0300

    Refactor: Ciclo do programa movido para a classe App

--- core/app.py
+++ core/app.py
@@ -1,8 +1,11 @@
+import signal
+
 from core.player import Player
 import util.motions as motions
 import util.commands as commands
 from util.screen import Screen
 from util import ui
+from util.keyboard import getch 
 from core.config import *
 from core.theme import set_theme
 
@@ -10,6 +13,7 @@ class App:
     def __init__(self):
         self.player = Player()
         self.screen = Screen()
+        self.dirty = True
 
         self.mode = "NORMAL"
         self.command = ""
@@ -23,7 +27,24 @@ class App:
         self.config = get_config()
         set_theme(self.config.general["theme"])
 
+    def run(self):
         ui.initscreen(self.screen)
+        signal.signal(signal.SIGWINCH, self.handle_resize)
+
+        try:
+            while self.running:
+                if self.dirty:
+                    self.draw()
+
+                key = getch()
+                self.handle_key(key)
+        finally:
+            ui.exitscreen()
+
+    def handle_resize(self, signum, frame):
+        self.screen.clear()
+        self.screen.resize()
+        self.draw()
 
     def handle_key(self, key):
         if self.mode == "NORMAL":
@@ -34,12 +55,13 @@ class App:
     def draw(self):
         ui.draw_background(self.screen)
         ui.draw_header(self.screen)
-        ui.draw_songs(self.screen, self.player.playlist, self.cursor, self.config.player["relativenumber"])
+        ui.draw_songs(self.screen, self.player.get_playlist(), self.cursor, self.config.player["relativenumber"])
         ui.draw_warning(self.screen, self.player.state)
-        ui.draw_statusbar(self.screen, self.mode, self.cursor+1, len(self.player.playlist))
+        ui.draw_statusbar(self.screen, self.mode, self.cursor+1, len(self.player.get_playlist()))
         ui.draw_commandline(self.screen, self.command)
 
         self.screen.render()
+        self.dirty = True
 
     def exit(self):
         self.player.exit()
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import sys
 from core.app import App
-from util.keyboard import getch
-
-import signal
 
 app = App()
 
@@ -10,21 +7,4 @@ if len(sys.argv) > 1:
     path = sys.argv[1]
     app.player.load_songs(path)
 
-def handle_resize(sigun, frame):
-    app.screen.clear()
-    app.screen.resize()
-    app.draw()
-
-signal.signal(signal.SIGWINCH, handle_resize)
- 
-while app.running:
-
-    if app.screen.check_resize():
-        app.screen.resize()
-
-    app.draw()
-
-    key = getch()
-
-    app.handle_key(key)
-
+app.run()
