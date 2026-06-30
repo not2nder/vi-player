e12950b2935156e58a0f4f4e67aff0f436cc5612
not2nder <brtiodark@gmail.com>
2026-06-29 23:23:27 -0300

    Feat: --version and --help flags

--- src/vi_player/main.py
+++ src/vi_player/main.py
@@ -1,14 +1,34 @@
-import sys
+import argparse
 from .core.app import App
 from .config.bootstrap import ensure_config
+from . import __version__
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog = "viplay",
+        description="A Vim-like music player for the terminal.",
+    )
+
+    parser.add_argument(
+        "directory",
+        nargs ="?",
+        help  = "music directory to open", 
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action  = "version",
+        version = f"VI-PLAYER {__version__}"
+    )
+
+    args = parser.parse_args()
+
     ensure_config()
     app = App()
 
-    if len(sys.argv) > 1:
-        path = sys.argv[1]
-        app.mpv.playlist.load_directory(path)
+    if args.directory:
+        app.mpv.playlist.load_directory(args.directory)
 
     app.run()
 
