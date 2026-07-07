93841d0f56a74174a7372a5cd817605269ea384d
not2nder <brtiodark@gmail.com>
2026-07-07 12:13:33 -0300

    Feat: Enable terminal mouse reporting

--- src/vi_player/util/ui.py
+++ src/vi_player/util/ui.py
@@ -2,23 +2,20 @@ from vi_player.util.pretty import *
 from vi_player.core.theme import get_theme 
 
 def initscreen(screen: object):
-    sys.stdout.write("\x1b[?1049h")
-    sys.stdout.write(hide_cursor())
-    sys.stdout.write("\x1b[H")
+    sys.stdout.write("\x1b[?1049h") # alternate screen
+    sys.stdout.write("\x1b[?25h")   # hide text cursor
+    sys.stdout.write("\x1b[?1000h") # activate mouse reporting
+    sys.stdout.write("\x1b[?1006h") # sgr mouse mode
     draw_background(screen)
     sys.stdout.flush()
 
 def exitscreen():
     sys.stdout.write("\x1b[0m")
-    sys.stdout.write(show_cursor())
+    sys.stdout.write("\x1b[?25h")
     sys.stdout.write("\x1b[?1049l")
+    sys.stdout.write("\x1b[?1000l")
+    sys.stdout.write("\x1b[?1006l")
     sys.stdout.flush()
-
-def show_cursor():
-    return "\x1b[?25h"
-
-def hide_cursor():
-    return "\x1b[?25l"
 
 def draw_background(screen: object):
     theme = get_theme()
@@ -27,4 +24,4 @@ def draw_background(screen: object):
     line = bg(normal["bg"]) + (" "*screen.width) + RESET
 
     for y in range(screen.height):
-        screen.draw(y+1,line)
+        screen.draw(y + 1,line)
